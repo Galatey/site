fe6f89ec87c3e7c711e20d69e2f4577dd27b6ec8
Galatey <John88power@gmail.com>
2023-02-15 22:32:28 +0300

    v.1

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  ssr: false,
-  nitro: {
-    preset: 'node-server'
-  },
   modules: [
     '@element-plus/nuxt'
   ],
