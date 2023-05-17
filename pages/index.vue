ca454f96525ef74be020fa0dbca207d3d71e2e10
Galatey <John88power@gmail.com>
2023-05-17 16:23:24 +0300

    11

--- pages/index.vue
+++ pages/index.vue
@@ -42,7 +42,33 @@
       <el-image style="width: 100%; height: 100%" src="https://avatars.mds.yandex.net/i?id=0440b1bed4fc89f11a85a15ce6f66e6792d63e12-7546644-images-thumbs&n=13" fit="cover" />
     </div>
   </div>
-  
+  <el-divider class="divider" />
+  <div class="otdelka">
+    <div class="ottdelka_foto">
+      <el-image style="width: 100%; height: 100%" src="https://avatars.mds.yandex.net/i?id=0440b1bed4fc89f11a85a15ce6f66e6792d63e12-7546644-images-thumbs&n=13" fit="cover" />
+    </div>
+    <div class="otdelka_info">
+      <p>Внешняя и внутренняя отделка любого уровня сложности</p>
+    </div>
+  </div>
+  <el-divider class="divider" />
+  <div class="stroy">
+    <div class="stroy_info">
+      <p>Строительство домов, бань и коттеджей</p>
+    </div>
+    <div class="stroy_foto">
+      <el-image style="width: 100%; height: 100%" src="https://avatars.mds.yandex.net/i?id=0440b1bed4fc89f11a85a15ce6f66e6792d63e12-7546644-images-thumbs&n=13" fit="cover" />
+    </div>
+  </div>
+  <el-divider class="divider" />
+  <div class="otdelka">
+    <div class="ottdelka_foto">
+      <el-image style="width: 100%; height: 100%" src="https://avatars.mds.yandex.net/i?id=0440b1bed4fc89f11a85a15ce6f66e6792d63e12-7546644-images-thumbs&n=13" fit="cover" />
+    </div>
+    <div class="otdelka_info">
+      <p>Внешняя и внутренняя отделка любого уровня сложности</p>
+    </div>
+  </div>
   <div class="carusel">
     <el-carousel :interval="8000" type="card">
       <el-carousel-item v-for="item in products" :key="item.id">
@@ -61,10 +87,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 import ModalWindow from '@/components/ModalWindow.vue';
 
-const onEntry = (entry) => {
+onMounted(() => {
+  const onEntry = (entry) => {
   entry.forEach(change => {
     if (change.isIntersecting) {
      change.target.classList.add('stroy-show');
@@ -74,13 +101,15 @@ const onEntry = (entry) => {
   });
 } 
 let options = {
-  threshold: [0.5] };
+threshold: [0.6] };
 let observer = new IntersectionObserver(onEntry, options);
 let elements = document.querySelectorAll('.stroy, .otdelka');
 
 for (let elm of elements) {
   observer.observe(elm);
 }
+})
+
 const show = ref(false)
 const pic = ref({})
 const products = ref([
@@ -88,7 +117,6 @@ const products = ref([
   { id: 2, image: "https://avatars.mds.yandex.net/i?id=f1be1d0bdf24700090478f21ca719288daced805-8497203-images-thumbs&n=13", title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam modi doloribus esse expedita voluptatum illo suscipit veniam. Quas, voluptatibus sunt. Quibusdam a inventore qui totam vitae, facilis eaque et est, libero ipsum impedit! Deleniti magni cupiditate ipsa odio officiis autem, repellendus doloribus voluptatum dolorem ad. Cum maxime optio dolorem quo blanditiis rerum soluta quis at explicabo harum unde adipisci aliquid accusamus ipsa nam ullam eveniet doloribus voluptas, sunt necessitatibus ratione obcaecati. Debitis consectetur officiis, quo repellat, distinctio vitae reiciendis at magni sapiente hic odit quis aliquam dignissimos similique temporibus iusto beatae fugiat quos culpa ex dolorem ratione nostrum. Eum nostrum sapiente officia delectus laboriosam suscipit voluptatibus a. Sequi voluptatem quis ratione perspiciatis mollitia vel maiores asperiores quisquam animi eos eligendi consequuntur, dolores sint inventore dolore alias temporibus minima iste incidunt accusamus vero sit! Exercitationem, ullam. Commodi perspiciatis a magni dolore ducimus sequi iure similique, tempora quos ipsum, aperiam assumenda?" },
   { id: 1, image: "https://avatars.mds.yandex.net/i?id=f1f6d9a5dbc28778b8423f9ad69b9f23dddb0304-7464698-images-thumbs&n=13", title: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam quibusdam modi doloribus esse expedita voluptatum illo suscipit veniam. Quas, voluptatibus sunt. Quibusdam a inventore qui totam vitae, facilis eaque et est, libero ipsum impedit! Deleniti magni cupiditate ipsa odio officiis autem, repellendus doloribus voluptatum dolorem ad. Cum maxime optio dolorem quo blanditiis rerum soluta quis at explicabo harum unde adipisci aliquid accusamus ipsa nam ullam eveniet doloribus voluptas, sunt necessitatibus ratione obcaecati. Debitis consectetur officiis, quo repellat, distinctio vitae reiciendis at magni sapiente hic odit quis aliquam dignissimos similique temporibus iusto beatae fugiat quos culpa ex dolorem ratione nostrum. Eum nostrum sapiente officia delectus laboriosam suscipit voluptatibus a. Sequi voluptatem quis ratione perspiciatis mollitia vel maiores asperiores quisquam animi eos eligendi consequuntur, dolores sint inventore dolore alias temporibus minima iste incidunt accusamus vero sit! Exercitationem, ullam. Commodi perspiciatis a magni dolore ducimus sequi iure similique, tempora quos ipsum, aperiam assumenda?" },
 ])
-// const { data: products } = await useFetch('https://fakestoreapi.com/products?limit=5')
 </script>
 
   
@@ -135,6 +163,7 @@ const products = ref([
   width: 70%;
   font-size: 38px;
   padding-top: 2%;
+  margin-right: 35px;
 }
 
 .stroy_foto {
@@ -202,10 +231,65 @@ const products = ref([
   background-color: transparent;
 }
 
-@media screen and (max-width: 400px) {
+@media screen and (max-width: 450px) {
   .stroy_info {
     font-size: 16px;
-  padding-top: 80px;
+    padding-top: 2%;
+}
+}
+
+@media screen and (max-width: 450px) {
+  .otdelka_info {
+    font-size: 16px;
+    padding-top: 2%;
+}
+}
+
+@media screen and (max-width: 550px) {
+  .stroy_info {
+    font-size: 20px;
+    padding-top: 2%;
+    margin-right: 15px;
+}
+}
+
+@media screen and (max-width: 550px) {
+  .otdelka_info {
+    font-size: 20px;
+    padding-top: 2%;
+    margin-left: 15px;
+}
+}
+
+@media screen and (max-width: 700px) {
+  .stroy_info {
+    font-size: 22px;
+    padding-top: 2%;
+    margin-right: 15px;
+}
+}
+
+@media screen and (max-width: 700px) {
+  .otdelka_info {
+    font-size: 22px;
+    padding-top: 2%;
+    margin-left: 15px;
+}
+}
+
+@media screen and (max-width: 900px) {
+  .stroy_info {
+    font-size: 26px;
+    padding-top: 2%;
+    margin-right: 15px;
+}
+}
+
+@media screen and (max-width: 900px) {
+  .otdelka_info {
+    font-size: 26px;
+    padding-top: 2%;
+    margin-left: 15px;
 }
 }
 
